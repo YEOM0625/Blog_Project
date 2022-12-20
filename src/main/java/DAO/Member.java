c6132f6358c88e7f22dc876604a7a4bcb011281e
YEOM0625 <109888011+YEOM0625@users.noreply.github.com>
2022-12-20 16:47:02 +0900

    시큐리티,멤버 클래스 생성
    .

---
+++ src/main/java/DAO/Member.java
@@ -0,0 +1,51 @@
+package DAO;
+
+public class Member {
+	private String id, name, pwd, tel, email,nickname;
+	private int regdate;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public int getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(int regdate) {
+		this.regdate = regdate;
+	}
+	
+	
+	
+}
